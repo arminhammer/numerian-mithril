2cfc8336703789f54aad28a04731467dcd91f9ff
arminhammer <arminhammer@gmail.com>
2015-06-11 22:47:24 -0400

    Getting POST to work.

--- server/server.js
+++ server/server.js
@@ -3,10 +3,25 @@
  */
 
 var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+
 var app = express();
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(multer()); // for parsing multipart/form-data
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.post('/', function (req, res) {
+
+  var body = req.body.file;
+  console.log(req);
+  res.send(body);
+
 });
 
 var server = app.listen(3000, function () {
